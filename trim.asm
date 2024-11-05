97220a49984ab5ccfd55169cd7250c7b65401b08
David S. Madole <david@madole.net>
2024-11-05 13:00:21 -0500

    Update repository path

--- trim.asm
+++ trim.asm
@@ -36,12 +36,12 @@ d_idewrite: equ   044ah
  
 begin:      br    start
 
-            db    7+80h
-            db    29
-            dw    2023
-            dw    1
+            db    11+80h
+            db    5
+            dw    2024
+            dw    2
 
-            db    'See github/dmadole/Elfos-trim for more information',0
+            db    'See github/dmadole/MiniDOS-trim for more information',0
 
 
 start:      ldi   1                     ; flag to automatically trim
